f2f42094912a4b692f42fd7388b3598728ef8da6
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-02-07 22:05:51 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Many Big (and small!) Things";
 
-    public const string Description = "Allows you to change the scale of towers. Doesn't affect sub towers.";
+    public const string Description = "Allows you to change the scale of towers. Doesn't affect sub towers. For mod ideas contact me on discord @lerpmcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "ManyBigThings"; // TODO add your repo name here, also in the download url in README.md
